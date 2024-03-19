a69a86cbc61b07d426824cf46cf876b283242b9c
shubhambhole99 <shubhambhole68@gmail.com>
2024-03-19 20:49:28 +0530

    f

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const path = require("path")
 process.env.NODE_ENV = "test"
 
 const data = dotenv.config({ path: path.join(__dirname, `../environment/.${process.env.NODE_ENV}.env`) })
-
+// console.log(process.env.EMAIL,process.env.PASSWORD)
 module.exports = {
     PORT: process.env.PORT,
     mongodburl: process.env.mongodburl,
@@ -12,6 +12,9 @@ module.exports = {
     JWT: process.env.JWt,
     accessKeyId:process.env.AWS_ACCESS_KEY,
     secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+    EMAIL:process.env.EMAIL,
+    PASSWORD:process.env.PASSWORD
 }
 
 console.log(data)
+
--- controller/contact/contactController.js
+++ controller/contact/contactController.js
@@ -1,10 +1,48 @@
 const Contact = require('../../models/Contact/contact');
+const nodemailer = require('nodemailer');
+let {EMAIL,PASSWORD}=require('../../config/config.js')
+
+// Send a mail to the new created contact
+// Function to send email
+
+const sendEmail = async (contact) => {
+    try {
+        let config={
+            service:'gmail',
+            auth:{
+                user:EMAIL,
+                pass:PASSWORD
+        
+            }
+        }
+        let transporter = nodemailer.createTransport(config)
+       
+      
+        const info = await transporter.sendMail({
+                    from: EMAIL, // sender address
+                    to: contact.email, // list of receivers
+                    subject: "Hello ✔", // Subject line
+                    text: "Hello world?", // plain text body
+                    html: '<p>Shubham HI</p>', // html body
+        });
+         
+        
+        
+        console.log("Message sent: %s", info.messageId,nodemailer.getTestMessageUrl(info));
+          // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email
+
+        console.log('Email sent successfully');
+    } catch (error) {
+        // console.log('Error sending email:', error);
+    }
+};
 
 // Create a new contact
 const createContact = async (req, res) => {
     try {
         const contact = new Contact(req.body);
         await contact.save();
+        await sendEmail(contact);
         res.status(201).json(contact);
     } catch (error) {
         res.status(500).json({ error: 'Failed to create contact' });
